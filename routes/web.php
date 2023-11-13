9518f05ca36213693a50d2971641db0cc8b515e0
rifqiafr <rifqiaf7@gmail.com>
2023-11-13 12:37:25 +0700

    Bootstrap Mazer & Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MahasiswaController;
+// use App\Http\Controllers\MahasiswaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +19,25 @@ use App\Http\Controllers\MahasiswaController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('books', BookController::class);
 Route::resource('authors', AuthorController::class);
